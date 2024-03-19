0e5e95e9cbddda5400c73d3007f305582463eedf
JinKristy <kristyjinny@gamil.com>
2024-03-19 10:18:01 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Box from './component/Box';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <Box title="player" />
+        <Box title="computer" />
+      </div>
+      <div className="button-wrap">
+        <button>가위</button>
+        <button>바위</button>
+        <button>보</button>
+      </div>
+    </>
   );
 }
 
---
+++ src/component/Box.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Box = () => {
+  return (
+    <div>
+      <h1>you</h1>
+      <img src='' alt=''></img>
+      <h2></h2>
+    </div>
+  )
+}
+
+export default Box
