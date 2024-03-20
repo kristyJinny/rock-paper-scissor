6fce5bc4ef0f907d85a203382ecf540ae1e78ffa
JinKristy <kristyjinny@gamil.com>
2024-03-20 21:00:49 +0900

    가위바위보 게임

--- src/App.js
+++ src/App.js
@@ -8,34 +8,71 @@ const choice = {
     img: "https://nationaltoday.com/wp-content/uploads/2021/08/National-Pet-Rock-Day-1200x834.jpg"
   },
   scissors : {
-    name: "scissors",
+    name: "Scissors",
     img: "https://cdn.imweb.me/thumbnail/20200514/7fc8b1411fa8d.png",
   },
   paper : {
-    name: "paper",
+    name: "Paper",
     img: "https://w7.pngwing.com/pngs/406/64/png-transparent-white-paper-illustration-black-and-white-line-art-post-it-note-sticky-note-angle-white-text.png",
   },
+  default: {
+    name: "버튼 클릭: 게임 시작!",
+    img: "https://img.freepik.com/premium-vector/hands-playing-rock-paper-scissors-game-flat-design-style-vector-illustration_540284-598.jpg",
+  }
 }
 
 function App() {
   
-  const [userSelect, setUserSelect] = useState(null);
-  const [computerSelect, setcomputerSelect] = useState(null);
+  const [userSelect, setUserSelect] = useState(choice.default);
+  const [computerSelect, setcomputerSelect] = useState(choice.default);
+  const [result, setResult] = useState("");
+
   const play = (userChoice) => {
     // console.log("clicked", userChoice);
-    setUserSelect(choice[userChoice]) 
+    setUserSelect(choice[userChoice])
+    let computerChoice = randomChoice()
+    setcomputerSelect(computerChoice);
+    setResult( judgement(choice[userChoice], computerChoice))
   };
+
+  const judgement = (user, computer) => {
+    //console.log("user", user, "computer", computer);
+    // 가위 바위 보 로직
+    if(user.name === computer.name) {
+      return "tie"
+    } else if (user.name === "Rock")
+      return computer.name === "Scissors" ? "win" : "lose";
+    else if (user.name === "Scissors")
+      return computer.name === "Paper" ? "win" : "lose";
+    else if (user.name === "Paper")
+    return computer.name === "Rock" ? "win" : "lose";
+  }
+  
+  const randomChoice=()=> {
+    /**
+     * Object.keys()
+     * 객체의 Key값을 가져와 배열로 만들어주는 메소드
+     */
+
+    let itemArray = Object.keys(choice);  // 객체에 키 값만 뽑아서 array 로 만들어 준다
+    //console.log("item array", itemArray);
+    let randomItem = Math.floor( Math.random()*itemArray.length);
+    let final = itemArray[randomItem];
+    return choice[final];
+    // console.log("final", final);
+    // console.log("random value", randomItem)
+  }
 
   return (
     <>
-      <div className="container">
-        <Box title="You" item={userSelect} />
-        <Box title="computer" item={computerSelect} />
-      </div>
       <div className="button-area">
         <button onClick={() => play("scissors")}>가위</button>
         <button onClick={() => play("rock")}>바위</button>
         <button onClick={() => play("paper")}>보</button>
+      </div>
+      <div className="container">
+        <Box title="You" item={userSelect} result={result} />
+        <Box title="Computer" item={computerSelect} result={result}/>
       </div>
     </>
   );
--- src/component/Box.js
+++ src/component/Box.js
@@ -1,13 +1,26 @@
 import React from 'react'
 
 const Box = (props) => {
-  console.log("props", props);
+  //console.log("props", props);
+
+  let result;
+  if (
+    props.title === "Computer" && // 카드가 컴퓨터 카드인가? &&
+    props.result !== "tie" && // 결과가 비긴 건가? &&
+    props.result !== ""  // props.result에 값이 있는가?
+  ) {
+    result = props.result === "win" ? "lose" : "win";
+  } else {
+    result = props.result;
+  }
+
 
   return (
-    <div className="box">
+    <div className={`box ${result}`}>
       <h1>{props.title}</h1>
+      <h2>{props.item && props.item.name}</h2>
       <img className="item-img" src={props.item && props.item.img} alt="이미지"></img>
-      <h2>WIN</h2>
+      <h2>{result}</h2>
     </div>
   )
 }
